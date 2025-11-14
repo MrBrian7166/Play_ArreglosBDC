48c461ec82398ed7a01ee259b653776240082e00
MrBrian7166 <166641818+MrBrian7166@users.noreply.github.com>
2025-11-14 13:55:23 -0600

    Fix comment formatting in JugandoArreglos.java

--- jugandoArreglos/src/main/java/itszn/mx/jugandoarreglos/JugandoArreglos.java
+++ jugandoArreglos/src/main/java/itszn/mx/jugandoarreglos/JugandoArreglos.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-/*Probando we  bhook */
+/*Probando we    bhook */
 /*Huevo *//*Frijoles*/
 
 package itszn.mx.jugandoarreglos;
